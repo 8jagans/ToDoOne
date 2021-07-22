585fc59e8d3e4bfd6a205fa2018a499ac046a3c4
8jagans <8jagans@gmail.com>
2021-07-22 10:16:58 +0530

    folders added

---
+++ src/Components/AllU/AllUsers.js
@@ -0,0 +1,16 @@
+import React, {Components} from 'react';
+import './AllUsers.css';
+
+
+
+
+function AllU() {
+    return (       
+        <div className="Header">
+            <h1> ToDoOne </h1>
+        </div>
+    );
+  }
+  
+export default AllU;
+  
